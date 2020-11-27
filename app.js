721209c74dd00064fba10fb6d7cd6f48c01225ee
Katia <katia.punter@storyshareplatform.com>
2020-11-27 16:36:16 +0000

    Adds contacts to local storage and the logic for submit

--- app.js
+++ app.js
@@ -8,3 +8,53 @@ const street = document.getElementById('street');
 const town = document.getElementById('town');
 const country = document.getElementById('country');
 const list = document.getElementById('list');
+
+const localStorageContacts = JSON.parse(localStorage.getItem('contacts'));
+
+let contacts = localStorage.getItem('contacts') !== null ? localStorageContacts : [];
+
+contacts.forEach(item => {
+    const li = document.createElement('li');
+    li.appendChild(document.createTextNode(`${item.firstName}: ${item.email}`));
+    const text = document.createElement('p');
+    li.appendChild(text);
+    list.appendChild(li);
+  });
+
+const addNewContact = e => {
+    e.preventDefault();
+    const contact = {
+        firstName: firstName.value,
+        otherNames: otherNames.value,
+        secondName: secondName.value,
+        email: email.value,
+        telephone: telephone.value,
+        street: street.value,
+        town: town.value,
+        country: country.value
+    };
+    console.log(contact);
+    contacts.push(contact);
+    //   addContactToDOM(contact);
+    updateLocalStorage();
+    console.log(localStorage);
+    firstName.value = '';
+    otherNames.value = '';
+    secondName.value = '';
+    email.value = '';
+    telephone.value = '';
+    street.value = '';
+    town.value = '';
+    country.value ='';
+}
+
+// Update local storage with the new contact
+function updateLocalStorage() {
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+}
+
+const addContactToDOM = contact => {
+
+}
+
+form.addEventListener('submit', addNewContact);
